8f287b54242185cdd0a651af893168877d6ad1a3
Emil Joseph <emiljoseph89@gmail.com>
2022-04-02 16:27:12 -0600

    create file  Home page in /src/pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
         <Routes>
-          
+          <Route
+            path='/'
+            element={<Home />}
+          />
         </Routes>
       <Footer />
     </div>
